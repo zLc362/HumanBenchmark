b8fef0db7e36e1cb30601f7d2db3064c4e4f245d
zLc <pocevstefan@gmail.com>
2023-06-07 20:31:41 +0200

    Add project files.

---
+++ HumanBenchmark/Form1.cs
@@ -0,0 +1,15 @@
+namespace HumanBenchmark
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+    }
+}
