dd0933cae5771ea6f21d7fcde3c18e049b9a5c4e
zLc <pocevstefan@gmail.com>
2023-06-20 22:02:20 +0200

    Implemented more of Number Memory

--- HumanBenchmark/form_Number_Memory.Designer.cs
+++ HumanBenchmark/form_Number_Memory.Designer.cs
@@ -28,9 +28,15 @@
         /// </summary>
         private void InitializeComponent()
         {
+            components = new System.ComponentModel.Container();
             btn_Back = new Button();
             lbl_Center = new Label();
-            button1 = new Button();
+            btn_Start_Next = new Button();
+            progress_bar_back = new Panel();
+            progress_bar_front = new Panel();
+            tmr_countdown = new System.Windows.Forms.Timer(components);
+            tbx_number_enter = new TextBox();
+            lbl_highscore = new Label();
             SuspendLayout();
             // 
             // btn_Back
@@ -46,28 +52,81 @@
             btn_Back.Size = new Size(118, 118);
             btn_Back.TabIndex = 15;
             btn_Back.UseVisualStyleBackColor = false;
+            btn_Back.Click += btn_Back_Click;
             // 
             // lbl_Center
             // 
+            lbl_Center.Anchor = AnchorStyles.None;
             lbl_Center.Font = new Font("Segoe UI", 19.8000011F, FontStyle.Bold, GraphicsUnit.Point);
             lbl_Center.ForeColor = Color.White;
-            lbl_Center.Location = new Point(345, 274);
+            lbl_Center.Location = new Point(345, 131);
             lbl_Center.Name = "lbl_Center";
-            lbl_Center.Size = new Size(761, 170);
+            lbl_Center.Size = new Size(761, 313);
             lbl_Center.TabIndex = 0;
             lbl_Center.Text = "A number will briefly show on the screen\r\nAfter it dissapears, write down the number";
             lbl_Center.TextAlign = ContentAlignment.MiddleCenter;
             // 
-            // button1
+            // btn_Start_Next
             // 
-            button1.Anchor = AnchorStyles.None;
-            button1.FlatStyle = FlatStyle.Flat;
-            button1.Location = new Point(668, 473);
-            button1.Name = "button1";
-            button1.Size = new Size(94, 29);
-            button1.TabIndex = 16;
-            button1.Text = "Start";
-            button1.UseVisualStyleBackColor = true;
+            btn_Start_Next.Anchor = AnchorStyles.None;
+            btn_Start_Next.FlatAppearance.BorderSize = 0;
+            btn_Start_Next.FlatStyle = FlatStyle.Flat;
+            btn_Start_Next.Location = new Point(644, 459);
+            btn_Start_Next.Name = "btn_Start_Next";
+            btn_Start_Next.Size = new Size(157, 61);
+            btn_Start_Next.TabIndex = 1;
+            btn_Start_Next.Text = "Start";
+            btn_Start_Next.UseVisualStyleBackColor = true;
+            btn_Start_Next.Click += btn_Start_Next_Click;
+            btn_Start_Next.Paint += btn_Start_Next_Paint;
+            // 
+            // progress_bar_back
+            // 
+            progress_bar_back.Anchor = AnchorStyles.None;
+            progress_bar_back.BackColor = SystemColors.HotTrack;
+            progress_bar_back.Location = new Point(579, 419);
+            progress_bar_back.Name = "progress_bar_back";
+            progress_bar_back.Size = new Size(283, 23);
+            progress_bar_back.TabIndex = 17;
+            // 
+            // progress_bar_front
+            // 
+            progress_bar_front.Anchor = AnchorStyles.None;
+            progress_bar_front.BackColor = Color.White;
+            progress_bar_front.Location = new Point(579, 419);
+            progress_bar_front.Name = "progress_bar_front";
+            progress_bar_front.Size = new Size(283, 23);
+            progress_bar_front.TabIndex = 18;
+            // 
+            // tmr_countdown
+            // 
+            tmr_countdown.Interval = 10;
+            tmr_countdown.Tick += tmr_countdown_Tick;
+            // 
+            // tbx_number_enter
+            // 
+            tbx_number_enter.Anchor = AnchorStyles.None;
+            tbx_number_enter.BackColor = SystemColors.MenuHighlight;
+            tbx_number_enter.BorderStyle = BorderStyle.None;
+            tbx_number_enter.Font = new Font("Segoe UI", 24F, FontStyle.Regular, GraphicsUnit.Point);
+            tbx_number_enter.Location = new Point(361, 281);
+            tbx_number_enter.Name = "tbx_number_enter";
+            tbx_number_enter.Size = new Size(728, 54);
+            tbx_number_enter.TabIndex = 18;
+            tbx_number_enter.TextAlign = HorizontalAlignment.Center;
+            tbx_number_enter.KeyPress += tbx_number_enter_KeyPress;
+            // 
+            // lbl_highscore
+            // 
+            lbl_highscore.Anchor = AnchorStyles.Bottom | AnchorStyles.Right;
+            lbl_highscore.AutoSize = true;
+            lbl_highscore.Font = new Font("Segoe UI", 26F, FontStyle.Regular, GraphicsUnit.Point);
+            lbl_highscore.Location = new Point(1262, 669);
+            lbl_highscore.Name = "lbl_highscore";
+            lbl_highscore.Size = new Size(160, 60);
+            lbl_highscore.TabIndex = 19;
+            lbl_highscore.Text = "Level 0";
+            lbl_highscore.Paint += lbl_highscore_Paint;
             // 
             // form_Number_Memory
             // 
@@ -75,18 +134,31 @@
             AutoScaleMode = AutoScaleMode.Font;
             BackColor = SystemColors.MenuHighlight;
             ClientSize = new Size(1434, 741);
-            Controls.Add(button1);
+            Controls.Add(progress_bar_front);
+            Controls.Add(lbl_highscore);
+            Controls.Add(tbx_number_enter);
+            Controls.Add(progress_bar_back);
+            Controls.Add(btn_Start_Next);
             Controls.Add(lbl_Center);
             Controls.Add(btn_Back);
             Name = "form_Number_Memory";
             Text = "form_Number_Memory";
+            WindowState = FormWindowState.Maximized;
+            FormClosing += form_Number_Memory_FormClosing;
+            Paint += form_Number_Memory_Paint;
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
 
         private Button btn_Back;
         private Label lbl_Center;
-        private Button button1;
+        private Button btn_Start_Next;
+        private Panel progress_bar_back;
+        private Panel progress_bar_front;
+        private System.Windows.Forms.Timer tmr_countdown;
+        private TextBox tbx_number_enter;
+        private Label lbl_highscore;
     }
 }
--- HumanBenchmark/form_Number_Memory.cs
+++ HumanBenchmark/form_Number_Memory.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Plasmoid.Extensions;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,9 +13,164 @@ namespace HumanBenchmark
 {
     public partial class form_Number_Memory : Form
     {
+        public Random random = new Random();
+        public int numberToGuess { get; set; }
+        public int lowerBound { get; set; }
+        public int upperBound { get; set; }
+        public int level { get; set; }
+        public bool started { get; set; }
+        public bool enteringNumber { get; set; }
+        public String buttonDisplay { get; set; }
+
+
         public form_Number_Memory()
         {
             InitializeComponent();
+            started = false;
+            enteringNumber = false;
+            tbx_number_enter.Hide();
+            progress_bar_back.Hide();
+            progress_bar_front.Hide();
+            buttonDisplay = "Start";
+        }
+
+        private void tmr_countdown_Tick(object sender, EventArgs e)
+        {
+            if (progress_bar_front.Width < 1)
+            {
+                progress_bar_front.Width = progress_bar_back.Width;
+                tmr_countdown.Stop();
+                btn_Start_Next.Show();
+                lbl_Center.Hide();
+                tbx_number_enter.Show();
+                tbx_number_enter.Focus();
+                enteringNumber = true;
+                progress_bar_back.Hide();
+                progress_bar_front.Hide();
+                buttonDisplay = "Guess";
+                btn_Start_Next.Invalidate();
+                return;
+            }
+            progress_bar_front.Width = (int)(progress_bar_front.Width - (progress_bar_back.Width * 0.001));
+        }
+        private void StartUp()
+        {
+            started = true;
+
+            tmr_countdown.Start();
+            btn_Start_Next.Hide();
+            lowerBound = 1;
+            upperBound = 10;
+            level = 0;
+            numberToGuess = random.Next(lowerBound, upperBound);
+            lbl_Center.Text = numberToGuess.ToString();
+            progress_bar_back.Show();
+            progress_bar_front.Show();
+            buttonDisplay = "Next";
+            btn_Start_Next.Invalidate();
+        }
+
+        private void btn_Start_Next_Click(object sender, EventArgs e)
+        {
+            if (!started)
+            {
+                StartUp();
+            }
+            else
+            {
+                if (enteringNumber)
+                {
+                    enteringNumber = false;
+                    lbl_Center.Text = String.Format("Number:\r\n{0}\r\nYour Guess:\r\n{1}", numberToGuess, tbx_number_enter.Text);
+                    lbl_Center.Show();
+                    tbx_number_enter.Hide();
+
+                    if (numberToGuess.ToString().Equals(tbx_number_enter.Text))
+                    {
+                        level++;
+                        lowerBound *= 10;
+                        upperBound *= 10;
+                        buttonDisplay = "Next";
+                        btn_Start_Next.Invalidate();
+                    }
+                    else
+                    {
+                        started = false;
+                        buttonDisplay = "Start";
+                        btn_Start_Next.Invalidate();
+                    }
+                    tbx_number_enter.Text = String.Empty;
+                    numberToGuess = random.Next(lowerBound, upperBound);
+
+                }
+                else
+                {
+                    lbl_Center.Text = numberToGuess.ToString();
+                    tmr_countdown.Interval = (int)(tmr_countdown.Interval * 1.2);
+                    tmr_countdown.Start();
+                    btn_Start_Next.Hide();
+                    progress_bar_back.Show();
+                    progress_bar_front.Show();
+                }
+            }
+            lbl_highscore.Invalidate();
+            HighScores.updateNumber_Memory(level);
+        }
+
+        private void lbl_highscore_Paint(object sender, PaintEventArgs e)
+        {
+            Brush brush = new SolidBrush(Color.White);
+            e.Graphics.FillRoundedRectangle(brush, e.ClipRectangle, 15);
+            brush = new SolidBrush(Color.Black);
+            StringFormat format = new StringFormat();
+            format.Alignment = StringAlignment.Center;
+            format.LineAlignment = StringAlignment.Center;
+            e.Graphics.DrawString("Level " + level.ToString(), lbl_highscore.Font, brush, e.ClipRectangle, format);
+            brush.Dispose();
+        }
+
+        private void btn_Back_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void form_Number_Memory_FormClosing(object sender, FormClosingEventArgs e)
+        {
+
+        }
+
+        private void form_Number_Memory_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics g = e.Graphics;
+            if (tbx_number_enter.Visible)
+            {
+                g.DrawRoundedRectangle(new Pen(progress_bar_back.BackColor, 2), tbx_number_enter.Location.X - 5, tbx_number_enter.Location.Y - 5, tbx_number_enter.Width + 10, tbx_number_enter.Height + 10, 15);
+            }
+            else
+            {
+                g.DrawRoundedRectangle(new Pen(this.BackColor, 2), tbx_number_enter.Location.X - 5, tbx_number_enter.Location.Y - 5, tbx_number_enter.Width + 10, tbx_number_enter.Height + 10, 15);
+            }
+
+        }
+
+        private void btn_Start_Next_Paint(object sender, PaintEventArgs e)
+        {
+            Pen pen = new Pen(Color.Black, 2);
+            Brush brush = new SolidBrush(progress_bar_back.BackColor);
+            e.Graphics.FillRoundedRectangle(brush, e.ClipRectangle, 15);
+            brush = new SolidBrush(Color.White);
+            StringFormat format = new StringFormat();
+            format.Alignment = StringAlignment.Center;
+            format.LineAlignment = StringAlignment.Center;
+            e.Graphics.DrawString(buttonDisplay, btn_Start_Next.Font, brush, e.ClipRectangle, format);
+        }
+
+        private void tbx_number_enter_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == ((char)Keys.Enter))
+            {
+                btn_Start_Next_Click(null, null);
+            }
         }
     }
 }
